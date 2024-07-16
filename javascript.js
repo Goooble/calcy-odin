3998f8e98cac13b7331a47fe354ad5ca37058df6
Gooble <bivinbheemaiah@gmail.com>
2024-07-16 11:35:03 +0530

    Make single expression work without clearing display array

--- javascript.js
+++ javascript.js
@@ -3,25 +3,46 @@ let secondNumber;
 let operator;
 let displayNumContainer = [];
 let panelFontSize = 50;
-const numberPanel = document.querySelector('.number-panel');
-const numberGrid = document.querySelector('.number-grid');
-const displayPanel = document.querySelector('.display');
-numberGrid.addEventListener('click', (e) => {
-    if(e.target.className === 'number-button'){
-  displayNumContainer[displayNumContainer.length] = e.target.textContent;
-  displayRestrainer();
-  displayValue(displayNumContainer);
+let operatorPressed = false;
+let operatorIndex;
+
+const numberPanel = document.querySelector(".number-panel");
+const numberGrid = document.querySelector(".number-grid");
+const displayPanel = document.querySelector(".display");
+
+numberGrid.addEventListener("click", (e) => {
+  if (e.target.className === "number-button") {
+    displayValue(displayNumContainer, e);
+  }
+
+  if (e.target.className === "operator-button") {
+    if (!operatorPressed) {
+      operatorPressed = true;
+      firstNumber = +displayNumContainer.join("");
+      operatorIndex = displayNumContainer.length;
+      displayValue(displayNumContainer, e);
+      console.log(firstNumber);
     }
+  }
+
+  if (e.target.className === "equal-button") {
+    secondNumber = +displayNumContainer.slice(operatorIndex+1).join("");
+    numberPanel.textContent = operate(firstNumber, secondNumber, displayNumContainer[operatorIndex]);
+    console.log(operatorIndex);
+  }
 });
 
-function displayValue(array){
-    numberPanel.textContent = array.join("");
+//display array updates here
+function displayValue(array, e) {
+  displayNumContainer[displayNumContainer.length] = e.target.textContent;
+  numberPanel.textContent = array.join("");
+  displayRestrainer();
 }
 
-function displayRestrainer(){
-  if(numberPanel.offsetWidth/displayPanel.offsetWidth >= 0.85){
+function displayRestrainer() {
+  if (numberPanel.offsetWidth / displayPanel.offsetWidth >= 0.85) {
     panelFontSize -= 4;
-    numberPanel.style.fontSize =  `${(panelFontSize)}px`;
+    numberPanel.style.fontSize = `${panelFontSize}px`;
   }
 }
 
