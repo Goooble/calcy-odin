120b8acca425bdb686c2eee73f05af86c69ba167
Gooble <bivinbheemaiah@gmail.com>
2024-07-17 17:29:38 +0530

    fix bugs caused by negative numbers

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //one function one job
-//find out the core loop where you can update values
+//find out the core loop where you can update values(point of change)
 //for any repeated code(expressions or block of statements) create variables and functions
 //create visual clues to keep track of logic and stuff, like enabled and disabled
 let firstNumber;
@@ -46,7 +46,8 @@ function enableOperator() {
   opButtons.forEach((btn) => (btn.disabled = false));
 }
 
-clearButton.addEventListener('click', clearPanel);
+//point of change where everything majorly kicks off at
+clearButton.addEventListener("click", clearPanel);
 
 mainButtonCont.addEventListener("click", (e) => {
   checkForZero();
@@ -57,76 +58,80 @@ mainButtonCont.addEventListener("click", (e) => {
       displayValue();
       console.log("number pressed");
       break;
-    
-      case "operator-button":
+
+    case "operator-button":
       updateArray(e);
       displayValue();
-      if(operatorIndex !== -1 ){
+      if (operatorIndex !== -1) {
         calculate();
         updateArray(e);
         displayValue();
       }
       console.log("operator pressed");
       break;
-    
-      case "equal-button":
+
+    case "equal-button":
       console.log("equal pressed");
       calculate();
       displayValue();
       break;
   }
-  
-  operatorIndex= numContainer.findIndex((item) => operatorString.includes(item) === true);
+  //keep updating these variables after every click
+
+  operatorIndex = numContainer.findIndex((item, index, array) => {
+    if (index !== 0) {
+      return operatorString.includes(item) === true;
+    }
+  });
   toggleButtons();
   updateFirstNum();
   updateSecondNum();
+  console.log("-------------------");
 });
-
+//to enable and disable these buttons when they shoudnt be working
 function toggleButtons() {
   latestValue = numContainer[numContainer.length - 1]; //to find if a number, equal, decimal or operator was pressed last
   //operators
-  if (numString.includes(latestValue.slice(0,1))) {
+  if (numString.includes(latestValue)) {
     enableOperator();
   } else {
     disableOperator();
   }
   //equal
-  if (
-    numString.includes(latestValue.slice(0,1)) &&
-    operatorIndex !==-1) {
+  if (numString.includes(latestValue) && operatorIndex !== -1) {
     enableEqual();
   } else {
     disableEqual();
   }
 
-//decimal
+  //decimal
 
-console.log(`latestValue: ${latestValue}`);
-console.log( `operator index: ${operatorIndex}`)
+  console.log(`latestValue: ${latestValue}`);
+  console.log(`operator index: ${operatorIndex}`);
 }
 
-//number updates
+//number updates to prepare for calculation
 function updateSecondNum() {
-  if(operatorIndex !== -1)
-  {secondNumber = +numContainer.slice(operatorIndex+1).join("");}
+  if (operatorIndex !== -1) {
+    secondNumber = +numContainer.slice(operatorIndex + 1).join("");
+  }
   console.log(`second number: ${secondNumber}`);
 }
 
 function updateFirstNum() {
-  if (operatorIndex !==-1 && latestValue === numContainer[operatorIndex]) {
-  
-  firstNumber = +numContainer.slice(0, numContainer.length - 1).join("");
-  console.log(`firstNumber: ${firstNumber}`);
+  if (operatorIndex !== -1 && latestValue === numContainer[operatorIndex]) {
+    firstNumber = +numContainer.slice(0, numContainer.length - 1).join("");
+    console.log(`firstNumber: ${firstNumber}`);
+  }
 }
-}
-
+//updates displayArray and calls operate function
 function calculate() {
   let result = operate(firstNumber, secondNumber, numContainer[operatorIndex]);
   numContainer = [];
-  numContainer[0] = result.toString();
+  numContainer = result.toString().split("");
 }
 
-//display array is updated
+//display array is updated that also acts as a basis for toggling and where first num and second num are extracted from
 function updateArray(e) {
   numContainer.push(e.target.textContent);
 }
@@ -135,7 +140,7 @@ function displayValue() {
   numberPanel.textContent = numContainer.join("");
   displayRestrainer();
 }
-//clear button 
+//clear button
 function clearPanel() {
   console.clear();
   numContainer = [];
@@ -148,21 +153,25 @@ function clearPanel() {
   disableOperator();
   disableEqual();
 }
-
+//function to execute after bruh has been displayed
 function checkForZero() {
   if (numberPanel.textContent === "bruh") {
     clearPanel();
   }
 }
-
+//to limit the number of digits on teh panel
 function displayRestrainer() {
-  
-  if (numberPanel.offsetWidth / displayPanel.offsetWidth >= 0.85) {
-    panelFontSize -= 4;
+  let widthExceeded =
+    numberPanel.offsetWidth / displayPanel.offsetWidth >= 0.85;
+  while (widthExceeded) {
+    panelFontSize -= 1;
+    numberPanel.style.fontSize = `${panelFontSize}px`;
   }
-  numberPanel.style.fontSize = `${panelFontSize}px`;
+  if (!widthExceeded) {
+    numberPanel.style.fontSize = `50px`;
+  }
 }
-
+//math stuff
 function operate(num1, num2, op) {
   let result;
   switch (op) {
