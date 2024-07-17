b0f8f4a097052757dd0cb780219e3dc7e76ec426
Gooble <bivinbheemaiah@gmail.com>
2024-07-17 20:52:12 +0530

    update README to include lessons learnt

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//one function one job
+//one function one job: follow this very strictly
 //find out the core loop where you can update values(point of change)
 //for any repeated code(expressions or block of statements) create variables and functions
 //create visual clues to keep track of logic and stuff, like enabled and disabled
